92b101149c6de34d5b80fb5310fb0c11510cfb52
El-Jesule <gonzalezgomezjesus160619997@gmail.com>
2026-07-17 14:03:01 +0200

    feat(counting-sheeps): add js

---
+++ countingSheeps.js
@@ -0,0 +1,18 @@
+let list1 = [ true,  true,  true,  false, true,  true,  true,  true , true,  false, true,  false, true,  false, false, true , true,  true,  true,  true , false, false, true,  true ];
+let list2 = [false, false, false];
+
+function countSheeps(list) {
+  let sheeps = 0;
+  for (let animals of list) {
+    if (animal === true) {
+      sheeps++;
+    }
+  }
+  if (sheeps>0) {
+    console.log(`There are ${sheeps} sheep in total.`);
+  } else {
+    console.log("UPS!!! Wolfs eaten sheeps.");
+  }
+}
+countSheeps(list1);
+countSheeps(list2);
